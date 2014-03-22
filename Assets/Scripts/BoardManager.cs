2aa9304fcb672b628c3ec84ee5999203fb68f367
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2014-03-22 00:54:41 -0300

    Mecânica do jogo concluida.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class BoardManager : MonoBehaviour {
+public class BoardManager : MonoBehaviour 
+{
+    private Transform hole;
+    private bool isPause = false;
+    private float btnWidth;
+    private float btnHeight = 60.0f;
+    private float padding = 20.0f;
+    private float space = 3.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
+        hole = GameObject.Find("Hole").transform;
+        btnWidth = (Screen.width - padding) / 3.0f - space;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(padding / 2.0f, Screen.height - 100.0f, Screen.width - padding, 100.0f));
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (GUILayout.Button("Shuffle", GUILayout.Width(btnWidth), GUILayout.Height(btnHeight)))
+        {
+            hole.SendMessage("Shuffle");
+        }
+
+        if (GUILayout.Button((isPause ? "Resume" : "Pause"), GUILayout.Width(btnWidth), GUILayout.Height(btnHeight)))
+        {
+            isPause = !isPause;
+            if (isPause)
+                Time.timeScale = 0;
+            else
+                Time.timeScale = 1;
+        }
+
+        if (GUILayout.Button("Quit", GUILayout.Width(btnWidth), GUILayout.Height(btnHeight)))
+        {
+            Application.Quit();
+        }
+
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        GUILayout.EndArea();
+    }
+
 }
---
+++ Assets/Scripts/Hole.cs
@@ -0,0 +1,219 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour
+{
+    #region Fields
+    public float speed = 10.0f;
+    private RaycastHit hit;
+    private float distance = 10.0f;
+    private bool isMoving = false;
+    private Transform blockRef;
+    private Vector3 origin;
+    private Vector3 target;
+    private bool isShuffling = false;
+    public int moveTimes = 50;
+    private float startTimeMovement;
+    #endregion
+
+    #region Update
+    void Update()
+    {
+
+        if (isShuffling)
+        {
+            Shuffle();
+        }
+        else
+        {
+#if UNITY_EDITOR
+            DebugInputs();
+#endif
+        }
+
+        if (isMoving)
+        {
+            transform.position = Vector3.Lerp(origin, target, (Time.time - startTimeMovement) * (isShuffling ? 1000.0f : speed));
+            blockRef.position = Vector3.Lerp(target, origin, (Time.time - startTimeMovement) * (isShuffling ? 1000.0f : speed));
+            if (Vector3.Distance(transform.position, target) <= 0.0f)
+                isMoving = false;
+        }
+    }
+    #endregion
+
+    #region Shuffle board
+    void Shuffle()
+    {
+        if (!isShuffling)
+        {
+            isShuffling = true;
+            StartCoroutine("Shuffling");
+        }
+
+    }
+
+    IEnumerator Shuffling()
+    {
+        int countMovement = 0;
+        while (countMovement < moveTimes)
+        {
+            if (isMoving)
+                yield return null;
+
+            bool move = false;
+            switch (Random.Range(1, 5))
+            {
+                case 1:
+                    if (MoveBlockToUp())
+                        move = true;
+                    break;
+                case 2:
+                    if (MoveBlockToRight())
+                        move = true;
+                    break;
+                case 3:
+                    if (MoveBlockToDown())
+                        move = true;
+                    break;
+                case 4:
+                    if (MoveBlockToLeft())
+                        move = true;
+                    break;
+            }
+
+            if (move)
+            {
+                countMovement++;
+                yield return null;
+            }
+        }
+
+        isShuffling = false;
+    }
+    #endregion
+
+    #region Properties to check movement
+
+    bool CheckMovement(Vector3 direction)
+    {
+        if (Physics.Raycast(transform.position, direction, out hit, distance))
+        {
+            origin = transform.position;
+            blockRef = hit.transform;
+            target = blockRef.position;
+            return true;
+        }
+
+        return false;
+    }
+
+    bool HasAboveBlock
+    {
+        get
+        {
+            return CheckMovement(Vector3.up);
+        }
+    }
+
+    bool HasBelowBlock
+    {
+        get
+        {
+            return CheckMovement(Vector3.down);
+        }
+    }
+
+    bool HasLeftBlock
+    {
+        get
+        {
+            return CheckMovement(Vector3.left);
+        }
+    }
+
+    bool HasRightBlock
+    {
+        get
+        {
+            return CheckMovement(Vector3.right);
+        }
+        
+    }
+    #endregion
+
+    #region Movement
+    bool MoveBlockToUp()
+    {
+        if (isMoving)
+            return false;
+
+        if (HasBelowBlock)
+        {
+            startTimeMovement = Time.time;
+            isMoving = true;
+        }
+
+        return true;
+    }
+
+    bool MoveBlockToDown()
+    {
+        if (isMoving)
+            return false;
+
+        if (HasAboveBlock)
+        {
+            startTimeMovement = Time.time;
+            isMoving = true;
+        }
+
+        return true;
+    }
+
+    bool MoveBlockToLeft()
+    {
+        if (isMoving)
+            return false;
+
+        if (HasRightBlock)
+        {
+            startTimeMovement = Time.time;
+            isMoving = true;
+        }
+
+        return true;
+    }
+
+    bool MoveBlockToRight()
+    {
+        if (isMoving)
+            return false;
+
+        if (HasLeftBlock)
+        {
+            startTimeMovement = Time.time;
+            isMoving = true;
+        }
+
+        return true;
+    }
+    #endregion
+
+    #region Debug inputs
+    void DebugInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            MoveBlockToLeft();
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            MoveBlockToRight();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            MoveBlockToUp();
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            MoveBlockToDown();
+    }
+    #endregion
+}
+
