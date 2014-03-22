2b7e41cbae67563a51a4df642a91f70709272b7f
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2014-03-22 11:49:31 -0300

    Adicionado a lógica para checar se o jogo acabou.

---
+++ Assets/Scripts/BlockCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockCollider : MonoBehaviour 
+{
+    public int blockIndex;
+    public bool BlockIsRightPlace { get; set; }
+
+    void Start()
+    {
+        this.blockIndex = int.Parse(name.Replace("Collider", ""));
+        this.BlockIsRightPlace = true;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (BlockIsRightPlace)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+
+        Gizmos.DrawSphere(transform.position, 0.25f);
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (blockIndex.ToString().Equals(hit.name.Replace("Block","")))
+            BlockIsRightPlace = true;
+        else
+            BlockIsRightPlace = false;
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameOver : MonoBehaviour
+{
+    #region Events
+    public delegate void GameOverAction();
+    public static event GameOverAction OnGameOver;
+    #endregion
+
+    private List<BlockCollider> colliders;
+    private bool isShuffling = false;
+
+	void Start () 
+    {
+        colliders = new List<BlockCollider>();
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("BlockCollider");
+        foreach (GameObject go in gos)
+            colliders.Add(go.GetComponent<BlockCollider>());
+	}
+	
+	void LateUpdate () 
+    {
+        if (!isShuffling)
+        {
+            foreach (BlockCollider col in colliders)
+            {
+                if (!col.BlockIsRightPlace)
+                    return;
+
+                if (OnGameOver != null)
+                {
+                    Debug.Log("Game over");
+                    OnGameOver();
+                }
+            }
+        }
+	}
+
+    void OnEnable()
+    {
+        Hole.OnStartShuffle += OnStartShuffle;
+        Hole.OnEndShuffle += OnEndShuffle;
+    }
+
+    void OnDisable()
+    {
+        Hole.OnStartShuffle -= OnStartShuffle;
+        Hole.OnEndShuffle -= OnEndShuffle;
+    }
+
+    void OnStartShuffle()
+    {
+        isShuffling = true;
+    }
+
+    void OnEndShuffle()
+    {
+        isShuffling = false;
+    }
+}
